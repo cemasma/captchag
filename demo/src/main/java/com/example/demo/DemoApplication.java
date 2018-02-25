e38ec381e5f9d2335ecf8cdcf94019b7c21a87a4
Cem Asma <dev.cemas@gmail.com>
2018-02-25 06:22:33 +0300

    demo added

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+
+import io.github.cemasma.captchag.CaptchaGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+@SpringBootApplication
+@Controller
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	private CaptchaGenerator captchaGenerator;
+
+	public DemoApplication() {
+		captchaGenerator = new CaptchaGenerator(5);
+	}
+
+	@RequestMapping(path = "/test", method = RequestMethod.GET)
+	public String hello(Model model, HttpServletRequest request) throws IOException {
+		captchaGenerator = new CaptchaGenerator(5);
+		captchaGenerator.drawRandomCharacters();
+		captchaGenerator.save();
+
+		return "test";
+	}
+
+	@RequestMapping(path = "/test", method = RequestMethod.POST)
+	public String hello(@RequestParam(name = "captchaCode", required = true) String captchaCode) {
+
+		if(!captchaGenerator.check(captchaCode)) {
+			// return error
+			return "error";
+		}
+		return "redirect:test";
+	}
+
+	@GetMapping(value = "/image")
+	public @ResponseBody byte[] getImage(HttpServletRequest request) throws IOException {
+		return captchaGenerator.getImage();
+	}
+}
